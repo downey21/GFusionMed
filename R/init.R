25818ad8c52963d159b9879a1ace762404b209a4
“downey21” <downe7921@gmail.com>
2024-06-19 01:31:01 +0900

    edit space

--- R/init.R
+++ R/init.R
@@ -8,11 +8,11 @@ NULL
 
 .onLoad <- function(libname, pkgname) {
     suppressPackageStartupMessages({
-    if (!requireNamespace("RcppArmadillo", quietly = TRUE)) {
-        stop("RcppArmadillo package is required but not installed.")
-    }
-    if (!requireNamespace("RcppEigen", quietly = TRUE)) {
-        stop("RcppEigen package is required but not installed.")
-    }
+        if (!requireNamespace("RcppArmadillo", quietly = TRUE)) {
+            stop("RcppArmadillo package is required but not installed.")
+        }
+        if (!requireNamespace("RcppEigen", quietly = TRUE)) {
+            stop("RcppEigen package is required but not installed.")
+        }
     })
 }
